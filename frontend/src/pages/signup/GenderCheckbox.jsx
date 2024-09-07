8158707fa1c316503effeb4bd52757bb4b0db958
Arfat <kaziarfat55@gmail.com>
2024-09-07 11:41:02 +0530

    create gender check box

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,7 +1,28 @@
 import React from "react"
 
 const GenderCheckbox = () => {
-	return <div></div>
+	return (
+		<div className="flex mt-2 mb-1">
+			<div className="form-control flex justify-center items-center">
+				<label className="label gap-2 cursor-pointer">
+					<span className="mr-2">Male</span>
+					<input
+						type="checkbox"
+						className="checkbox border-slate-900"
+					/>
+				</label>
+			</div>
+			<div className="form-control flex justify-center items-center">
+				<label className="label gap-2 cursor-pointer ml-3">
+					<span>Female</span>
+					<input
+						type="checkbox"
+						className="checkbox border-slate-900"
+					/>
+				</label>
+			</div>
+		</div>
+	)
 }
 
 export default GenderCheckbox
