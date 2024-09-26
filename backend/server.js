4a64dcf88a23d0d3323ec49a1e79403da33275d4
arfat_kazi <arfatkazi115@gmail.com>
2024-09-26 12:37:59 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-import path from "path"
-import express from "express"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-import authRoutes from "./routes/auth.Routes.js"
-import messageRoutes from "./routes/message.Routes.js"
-import userRoutes from "./routes/user.Routes.js"
-import connectDB from "./config/db.js"
+import path from 'path'
+import express from 'express'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import authRoutes from './routes/auth.Routes.js'
+import messageRoutes from './routes/message.Routes.js'
+import userRoutes from './routes/user.Routes.js'
+import connectDB from './config/db.js'
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -17,18 +17,18 @@ app.use(cookieParser())
 // app.use(express.urlencoded({ extended: true }))
 
 //Defining routes
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
-app.use("/api/users", userRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/messages', messageRoutes)
+app.use('/api/users', userRoutes)
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")))
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
 })
 
 app.listen(port, () => {
-	//  DATABASE CONNECTION
-	connectDB()
-	console.log(`server is listening on ${port}`)
+  //  DATABASE CONNECTION
+  connectDB()
+  console.log(`server is listening on ${port}`)
 })
